8f1fab4edf05f1ec70a0c6ccfae30805154e4d5f
BrianWu <brianwu291@gmail.com>
2020-02-17 13:40:36 +0800

    refs #master, upload to github

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use(express.static('public'))
 app.get('/app/jsonp', (req, res) => {
   const responseData = { data: [{ id: 1, name: 'Brian' }, { id: 2, name: 'Peter' }] }
   const responseDataText = JSON.stringify(responseData)
+  res.set('Content-Type', 'application/text')
   res.send(`jsonpCallback(${responseDataText})`)
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log('Listening at ' + PORT)
 })
